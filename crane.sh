4922791a2764fba421a15699d48bad62bbbf9234
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2026-01-12 13:19:19 +0800

    优化crane.sh中的日志输出，增强函数调用的可读性

--- crane.sh
+++ crane.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
 
-set -e
+set -ex
+
+source /app/log.sh
 
 CraneDigest() {
 	local IMAGE=$1
+	log INFO "获取镜像哈希"
 	crane digest "$IMAGE" 2>/dev/null || true
 }
 
 CraneCopy() {
 	local SOURCE=$1
 	local TARGET=$2
+	log INFO "使用 crane copy 同步镜像"
 	crane copy --jobs 1 "$SOURCE" "$TARGET"
 }
 
 CraneAdvancedCopy() {
 	local SOURCE=$1
 	local TARGET=$2
-	local TEMPTAR=/app/temp.tar
-	crane pull "$SOURCE" "$TEMPTAR"
-	crane push "$TEMPTAR" "$TARGET"
-	rm -f "$TEMPTAR"
+	log INFO "使用 crane pull 和 push 同步镜像"
+	crane pull "$SOURCE" /app/temp.tar
+	crane push /app/temp.tar "$TARGET"
+	rm -f /app/temp.tar
 }
 
 CraneDebugCopy() {
 	local SOURCE=$1
 	local TARGET=$2
+	log INFO "使用 crane copy 调试模式同步镜像"
 	crane copy --debug --jobs 1 "$SOURCE" "$TARGET"
 }
